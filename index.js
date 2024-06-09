ffe9c8be228dbca42c376cb6762016499dddebb3
Joao Tomaz <joao@truecompliance.co.uk>
2024-06-09 03:07:33 +0100

    test the output

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ const fileName = core.getInput('file-name') || '.env';
 try {
   let envFileContent = '';
 
-  Object.keys(process.env).forEach(function(key) {
-    if(key.startsWith(inputPrefix)) {
-      envFileContent += `${key.substring(inputPrefix.length)}=${process.env[key]}\n`;
+//   Object.keys(process.env).forEach(function(key) {
+//     if(key.startsWith(inputPrefix)) {
+//       envFileContent += `${key.substring(inputPrefix.length)}=${process.env[key]}\n`;
+//     }
+//   });
+    const keys = Object.keys(process.env);
+    for (const key of keys) {
+        if (key && key.startsWith(inputPrefix)) {
+            console.log('Key', keys);
+            const keyName = key.substring(inputPrefix.length);
+            envFileContent += `${keyName}=${process.env[key]}\n`
+        }
     }
-  });
+    console.log(envFileContent);
 
-  fs.writeFile(fileName, envFileContent, function (error) {
+    fs.writeFile(fileName, envFileContent, function (error) {
     if (error) {
       core.setFailed(error.message);
     }
